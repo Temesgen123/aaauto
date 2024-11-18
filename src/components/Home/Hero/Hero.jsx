1aa4cfce2eb3f13b71844b04a719f4a40c919945
Temesgen Birechis <birechis@gmail.com>
2024-11-18 10:51:49 +0300

    changed hero image

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../../../assets/images/aiheroimage0001.png';
+import img from '../../../assets/images/heroImageBlue.webp';
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
