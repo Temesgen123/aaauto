e5ffb48d7acb5b3b647e351d33b74f4b32474d98
Temesgen Birechis <birechis@gmail.com>
2024-11-18 11:11:42 +0300

    restored background image

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../../../assets/images/heroImageBlue.webp';
+import img from '../../../assets/images/heroimage.png';
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
@@ -10,23 +10,23 @@ const Hero = () => {
         <div className="w-full md:w-2/4 space-y-5 md:mt-24 mt-10 relative px-6">
           <h1
             className="text-4xl lg:text-5xl font-bold leading-tight
-          md:bg-gradient-to-r from-black to-[#000] bg-clip-text md:text-transparent backdrop-opacity-50  text-[#010c42]"
+          md:bg-gradient-to-r from-white to-[#fff] bg-clip-text md:text-transparent backdrop-opacity-50  text-[#010c42]"
           >
             Drive into the future.
           </h1>
           {/* <div className='bg-[#fff] rounded-full w-16 h-16 absolute right-20 top-[25px] z-20 opacity-30 hover:blur-md shadow-[200px] hover:scale-150 ' >{}</div> */}
-          <h2 className=" text-xl lg:text-2xl font-bold text-[#010c42] md:text-black">
+          <h2 className=" text-xl lg:text-2xl font-bold text-[#010c42] md:text-white">
             Over 100+ New Cars Available Here.
           </h2>
           <p
             className="text-lg lg:text-base font-bold text-[#fc2b4b]
-          ] md:text-black"
+          ] md:text-white"
           >
             Explore the latest models and unbeatable offers.
           </p>
           <div className="flex gap-8 ">
             <Link to={'/aaauto/services'}>
-              <button className="bg-[#1b0459] py-1 px-4 rounded-md font-bold border-2 text-black hover:scale-95 hover:bg-white hover:text-[#1b0459] hover:border-blue-900 transition duration-400 ease-linear mt-5">
+              <button className="bg-[#1b0459] py-1 px-4 rounded-md font-bold border-2 text-white hover:scale-95 hover:bg-white hover:text-[#1b0459] hover:border-blue-900 transition duration-400 ease-linear mt-5">
                 Explor More
               </button>
             </Link>
