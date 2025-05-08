80127f4ff395c38bf10b47c755c005968f3d56bf
Temesgen Birechis <birechis@gmail.com>
2025-05-08 08:01:26 +0300

    edited hero section

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className="shadow-lg  heropage  h-[30vh] md:h-[90vh] relative md:bg-slate-100">
+    <div className="shadow-lg  heropage  h-[32vh] md:h-[80vh] relative md:bg-slate-100">
       <div className="container flex flex-col md:flex-row justify-left px-6 items-center">
         {/* Content section */}
         <div
-          className="w-full md:w-2/4 space-y-[2px] md:space-y-5 md:mt-24 mt-16 relative 
-        px-12 md:px-12"
+          className="w-full md:w-2/4 md:space-y-5 md:mt-24 mt-8 relative 
+        px-12 md:px-12 bg-red-500"
         >
           <h1
-            className="text-[16px] lg:text-5xl font-bold leading-tight
+            className="text-[18px] lg:text-5xl font-bold leading-tight
           lg:bg-gradient-to-r from-[#fff] to-[#faf9fc] bg-clip-text md:text-transparent backdrop-opacity-50  text-[#fff]"
           >
             Drive into the future.
           </h1>
           {/* <div className='bg-[#fff] rounded-full w-16 h-16 absolute right-20 top-[25px] z-20 opacity-30 hover:blur-md shadow-[200px] hover:scale-150 ' >{}</div> */}
-          <h2 className=" text-[8px] lg:text-2xl font-bold text-[#f4f4f6] md:text-[#f4f4f6]">
+          <h2 className=" text-[12px] lg:text-2xl font-bold text-[#f4f4f6] md:text-[#f4f4f6]">
             Over 100+ New Cars Available Here.
           </h2>
           <p
-            className="text-[6px] lg:text-base font-bold text-[#f4f4f6]
+            className="text-[10px] lg:text-base font-bold text-[#f4f4f6]
           ] md:text-[#f4f4f6]"
           >
             Explore the latest models and unbeatable offers.
