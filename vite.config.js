d891a957cdff4eab9480f096cd64060d436c0a02
Temesgen Birechis <birechis@gmail.com>
2025-06-03 14:36:37 +0300

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   base : "/aaauto/",
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  base : "/aaauto/",
-  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
+
